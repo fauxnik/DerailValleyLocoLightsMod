65432b3515b63caba9d98e1067548955ec0344f1
Niko Fox <niko@fauxnik.me>
2020-07-18 10:36:13 -0700

    refactor light setup a bit + DE6 front headlight aligned

--- LocoLightsMod/LocoLights.cs
+++ LocoLightsMod/LocoLights.cs
@@ -117,37 +117,13 @@ namespace LocoLightsMod
             if (car == null) { return; }
 
             // Front Head Light
-            GameObject FHL;
-            Renderer FHL_R;
-            Light FHL_Light;
+            GameObject go;
+            Renderer r;
+            Light l;
 
-            // Left Front Ditch Light
-            GameObject LFDL;
-            Renderer LFDL_R;
-            Light LFDL_Light;
-
-            // Right Front Ditch Light
-            GameObject RFDL;
-            Renderer RFDL_R;
-            Light RFDL_Light;
-
-            // Rear Head Light
-            GameObject RHL;
-            Renderer RHL_R;
-            Light RHL_Light;
-
-            // Left Rear Ditch Light
-            GameObject LRDL;
-            Renderer LRDL_R;
-            Light LRDL_Light;
-
-            // Right Rear Ditch Light
-            GameObject RRDL;
-            Renderer RRDL_R;
-            Light RRDL_Light;
-
-            float FDL_Offset;
-            float RDL_Offset;
+            float offsetX;
+            float offsetY;
+            float offsetZ;
             Vector3 euler;
 
             switch (car.carType)
@@ -159,30 +135,29 @@ namespace LocoLightsMod
                         new LocoLightData(LocoLightType.FHL, 4f, 1f, 2.25f)
                     }, new LocoLightData(LocoLightType.cab, 4f, 0.015f, 0.2f));
 
-                    FHL = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    FHL.name = LocoLightType.FHL.ToString();
-                    GameObject.Destroy(FHL.GetComponent<BoxCollider>());
-                    FHL.transform.position = car.transform.position;
-                    FHL.transform.rotation = car.transform.rotation;
-                    FHL.transform.localScale = new Vector3(0.28f, 0.28f, 0.05f);
-                    FHL_R = FHL.GetComponent<Renderer>();
-                    FHL_R.material.color = new Color32(255, 255, 255, 150);
-                    StandardShaderUtils.ChangeRenderMode(FHL_R.material, StandardShaderUtils.BlendMode.Transparent);
-                    FHL_R.enabled = false;
+                    // Front Head Light
+                    go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    go.name = LocoLightType.FHL.ToString();
+                    GameObject.Destroy(go.GetComponent<BoxCollider>());
+                    go.transform.position = car.transform.position;
+                    go.transform.rotation = car.transform.rotation;
+                    go.transform.localScale = new Vector3(0.28f, 0.28f, 0.05f);
+                    r = go.GetComponent<Renderer>();
+                    r.material.color = new Color32(255, 255, 255, 150);
+                    StandardShaderUtils.ChangeRenderMode(r.material, StandardShaderUtils.BlendMode.Transparent);
+                    r.enabled = false;
+                    l = go.AddComponent<Light>();
+                    l.shadows = LightShadows.Soft;
+                    l.type = LightType.Spot;
+                    l.spotAngle = 50;
+                    l.color = new Color32(255, 251, 225, 255);
+                    l.range = 26;
+                    l.enabled = false;
 
-                    FHL.transform.position += car.transform.forward * 10.96f;
-                    FHL.transform.position += car.transform.up * 3.58f;
+                    go.transform.position += car.transform.forward * 10.96f;
+                    go.transform.position += car.transform.up * 3.58f;
 
-                    FHL_Light = FHL.AddComponent<Light>();
-                    FHL_Light.shadows = LightShadows.Soft;
-                    FHL_Light.type = LightType.Spot;
-                    FHL_Light.spotAngle = 50;
-                    FHL_Light.color = new Color32(255, 251, 225, 255);
-                    FHL_Light.range = 26;
-                    FHL_Light.intensity = 2.25f;
-                    FHL_Light.enabled = false;
-
-                    FHL.transform.SetParent(car.transform, true);
+                    go.transform.SetParent(car.transform, true);
 
                     // default cab light: Color32(255, 253, 240, 255)
                     car.transform.Find("[cab light]").GetComponent<Light>().color = new Color32(255, 178, 63, 255);
@@ -198,63 +173,165 @@ namespace LocoLightsMod
                         new LocoLightData(LocoLightType.RRDL, 4f, 1f, 1.75f, 0.35f, 1f, 0.35f)
                     }, new LocoLightData(LocoLightType.cab, 2f, 0.075f, 0.1f));
 
-                    LFDL = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    LFDL.name = LocoLightType.LFDL.ToString();
-                    GameObject.Destroy(LFDL.GetComponent<BoxCollider>());
-                    LFDL.transform.position = car.transform.position;
-                    LFDL.transform.rotation = car.transform.rotation;
-                    LFDL.transform.localScale = new Vector3(0.21f, 0.21f, 0.05f);
-                    LFDL_R = LFDL.GetComponent<Renderer>();
-                    LFDL_R.material.color = new Color32(255, 255, 255, 150);
-                    StandardShaderUtils.ChangeRenderMode(LFDL_R.material, StandardShaderUtils.BlendMode.Transparent);
-                    LFDL_R.enabled = false;
+                    // Left Front Ditch Light
+                    go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    go.name = LocoLightType.LFDL.ToString();
+                    GameObject.Destroy(go.GetComponent<BoxCollider>());
+                    go.transform.position = car.transform.position;
+                    go.transform.rotation = car.transform.rotation;
+                    go.transform.localScale = new Vector3(0.21f, 0.21f, 0.05f);
+                    r = go.GetComponent<Renderer>();
+                    r.material.color = new Color32(255, 255, 255, 150);
+                    StandardShaderUtils.ChangeRenderMode(r.material, StandardShaderUtils.BlendMode.Transparent);
+                    r.enabled = false;
+                    l = go.AddComponent<Light>();
+                    l.shadows = LightShadows.Soft;
+                    l.type = LightType.Spot;
+                    l.spotAngle = 50;
+                    l.color = new Color32(255, 251, 225, 255);
+                    l.range = 26;
+                    l.enabled = false;
 
-                    FDL_Offset = 0.65f;
-                    LFDL.transform.position += car.transform.forward * 3.235f;
-                    LFDL.transform.position += car.transform.up * 1.528f;
-                    LFDL.transform.position += -car.transform.right * FDL_Offset;
+                    offsetX = 0.65f;
+                    go.transform.position += car.transform.forward * 3.235f;
+                    go.transform.position += car.transform.up * 1.528f;
+                    go.transform.position += -car.transform.right * offsetX;
 
-                    LFDL_Light = LFDL.AddComponent<Light>();
-                    LFDL_Light.shadows = LightShadows.Soft;
-                    LFDL_Light.type = LightType.Spot;
-                    LFDL_Light.spotAngle = 50;
-                    LFDL_Light.color = new Color32(255, 251, 225, 255);
-                    LFDL_Light.range = 26;
-                    LFDL_Light.intensity = 2.25f;
-                    LFDL_Light.enabled = false;
+                    go.transform.SetParent(car.transform, true);
 
-                    RFDL = GameObject.Instantiate(LFDL, LFDL.transform.position, LFDL.transform.rotation);
-                    RFDL.name = LocoLightType.RFDL.ToString();
-                    (RFDL_R = RFDL.GetComponent<Renderer>()).enabled = false;
-                    (RFDL_Light = RFDL.GetComponent<Light>()).enabled = false;
-                    RFDL.transform.position += car.transform.right * 2f * FDL_Offset;
+                    // Right Front Ditch Light
+                    go = GameObject.Instantiate(go, go.transform.position, go.transform.rotation);
+                    go.name = LocoLightType.RFDL.ToString();
+                    (r = go.GetComponent<Renderer>()).enabled = false;
+                    (l = go.GetComponent<Light>()).enabled = false;
 
-                    RDL_Offset = 0.65f;
+                    go.transform.position += car.transform.right * 2f * offsetX;
+
+                    go.transform.SetParent(car.transform, true);
+
+                    // Left Rear Ditch Light
                     euler = car.transform.rotation.eulerAngles;
-                    LRDL = GameObject.Instantiate(LFDL, car.transform.position, Quaternion.Euler(euler.x, euler.y + 180f, euler.z));
-                    LRDL.name = LocoLightType.LRDL.ToString();
-                    (LRDL_R = LRDL.GetComponent<Renderer>()).enabled = false;
-                    (LRDL_Light = LRDL.GetComponent<Light>()).enabled = false;
-                    LRDL.transform.position += -car.transform.forward * 3.227f;
-                    LRDL.transform.position += car.transform.up * 1.928f;
-                    LRDL.transform.position += -car.transform.right * RDL_Offset;
+                    go = GameObject.Instantiate(go, car.transform.position, Quaternion.Euler(euler.x, euler.y + 180f, euler.z));
+                    go.name = LocoLightType.LRDL.ToString();
+                    (r = go.GetComponent<Renderer>()).enabled = false;
+                    (l = go.GetComponent<Light>()).enabled = false;
 
-                    RRDL = GameObject.Instantiate(LRDL, LRDL.transform.position, LRDL.transform.rotation);
-                    RRDL.name = LocoLightType.RRDL.ToString();
-                    (RRDL_R = RRDL.GetComponent<Renderer>()).enabled = false;
-                    (RRDL_Light = RRDL.GetComponent<Light>()).enabled = false;
-                    RRDL.transform.position += car.transform.right * 2f * RDL_Offset;
+                    offsetX = 0.65f;
+                    go.transform.position += -car.transform.forward * 3.227f;
+                    go.transform.position += car.transform.up * 1.928f;
+                    go.transform.position += -car.transform.right * offsetX;
 
-                    LFDL.transform.SetParent(car.transform, true);
-                    RFDL.transform.SetParent(car.transform, true);
-                    LRDL.transform.SetParent(car.transform, true);
-                    RRDL.transform.SetParent(car.transform, true);
+                    go.transform.SetParent(car.transform, true);
+
+                    // Right Rear Ditch Light
+                    go = GameObject.Instantiate(go, go.transform.position, go.transform.rotation);
+                    go.name = LocoLightType.RRDL.ToString();
+                    (r = go.GetComponent<Renderer>()).enabled = false;
+                    (l = go.GetComponent<Light>()).enabled = false;
+
+                    go.transform.position += car.transform.right * 2f * offsetX;
+
+                    go.transform.SetParent(car.transform, true);
 
                     // default cab light: Color32(255, 253, 240, 255)
                     car.transform.Find("[cab light]").GetComponent<Light>().color = new Color32(255, 251, 225, 255);
                     break;
 
                 case TrainCarType.LocoDiesel:
+                    car.transform.gameObject.AddComponent<LocoLights>();
+                    car.transform.gameObject.GetComponent<LocoLights>().Init(car, new LocoLightData[]
+                    {
+                        new LocoLightData(LocoLightType.FHL, 4f, 1f, 1.75f, 1f, 0.35f, 0.35f),
+                        new LocoLightData(LocoLightType.LFDL, 4f, 1f, 1.75f, 1f, 0.35f, 0.35f),
+                        new LocoLightData(LocoLightType.RFDL, 4f, 1f, 1.75f, 1f, 0.35f, 0.35f),
+                        new LocoLightData(LocoLightType.RHL, 4f, 1f, 1.75f, 0.35f, 1f, 0.35f),
+                        new LocoLightData(LocoLightType.LRDL, 4f, 1f, 1.75f, 0.35f, 1f, 0.35f),
+                        new LocoLightData(LocoLightType.RRDL, 4f, 1f, 1.75f, 0.35f, 1f, 0.35f)
+                    }, new LocoLightData(LocoLightType.cab, 2f, 0f, 0f)); // 0.075f, 0.1f));
+
+                    // Front Head Light
+                    go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    go.name = LocoLightType.FHL.ToString();
+                    GameObject.Destroy(go.GetComponent<BoxCollider>());
+                    go.transform.position = car.transform.position;
+                    go.transform.rotation = car.transform.rotation;
+                    go.transform.localScale = new Vector3(0.19f, 0.19f, 0.05f);
+                    r = go.GetComponent<Renderer>();
+                    r.material.color = new Color32(255, 255, 255, 150);
+                    StandardShaderUtils.ChangeRenderMode(r.material, StandardShaderUtils.BlendMode.Transparent);
+                    //r.enabled = false;
+                    l = go.AddComponent<Light>();
+                    l.shadows = LightShadows.Soft;
+                    l.type = LightType.Spot;
+                    l.spotAngle = 50;
+                    l.color = new Color32(255, 251, 225, 255);
+                    l.range = 26;
+                    //l.enabled = false;
+
+                    offsetY = 3.765f;
+                    go.transform.position += car.transform.forward * 7.168f;
+                    go.transform.position += car.transform.up * offsetY;
+
+                    go.transform.SetParent(car.transform, true);
+
+                    // Left Front Ditch Light
+                    go = GameObject.Instantiate(go, go.transform.position, go.transform.rotation);
+                    go.name = LocoLightType.LFDL.ToString();
+                    (r = go.GetComponent<Renderer>()).enabled = false;
+                    (l = go.GetComponent<Light>()).enabled = false;
+
+                    offsetX = 0.65f;
+                    go.transform.position += car.transform.forward * 2.25f;
+                    go.transform.position += car.transform.up * (1.5f - offsetY);
+                    go.transform.position += -car.transform.right * offsetX;
+
+                    go.transform.SetParent(car.transform, true);
+
+                    // Right Front Ditch Light
+                    go = GameObject.Instantiate(go, go.transform.position, go.transform.rotation);
+                    go.name = LocoLightType.RFDL.ToString();
+                    (r = go.GetComponent<Renderer>()).enabled = false;
+                    (l = go.GetComponent<Light>()).enabled = false;
+
+                    go.transform.position += car.transform.right * 2f * offsetX;
+
+                    go.transform.SetParent(car.transform, true);
+
+                    // Rear Head Light
+                    euler = car.transform.rotation.eulerAngles;
+                    go = GameObject.Instantiate(go, car.transform.position, Quaternion.Euler(euler.x, euler.y + 180f, euler.z));
+                    go.name = LocoLightType.RHL.ToString();
+                    (r = go.GetComponent<Renderer>()).enabled = false;
+                    (l = go.GetComponent<Light>()).enabled = false;
+
+                    offsetY = 3.58f;
+                    go.transform.position += -car.transform.forward * 20.96f;
+                    go.transform.position += car.transform.up * offsetY;
+
+                    go.transform.SetParent(car.transform, true);
+
+                    // Left Rear Ditch Light
+                    go = GameObject.Instantiate(go, go.transform.position, go.transform.rotation);
+                    go.name = LocoLightType.LRDL.ToString();
+                    (r = go.GetComponent<Renderer>()).enabled = false;
+                    (l = go.GetComponent<Light>()).enabled = false;
+
+                    offsetX = 0.65f;
+                    go.transform.position += car.transform.up * (1.5f - offsetY);
+                    go.transform.position += -car.transform.right * offsetX;
+
+                    go.transform.SetParent(car.transform, true);
+
+                    // Right Rear Ditch Light
+                    go = GameObject.Instantiate(go, go.transform.position, go.transform.rotation);
+                    go.name = LocoLightType.RRDL.ToString();
+                    (r = go.GetComponent<Renderer>()).enabled = false;
+                    (l = go.GetComponent<Light>()).enabled = false;
+
+                    go.transform.position += car.transform.right * 2f * offsetX;
+
+                    go.transform.SetParent(car.transform, true);
+
                     // default cab light: Color32(255, 253, 240, 255)
                     car.transform.Find("[cab light]").GetComponent<Light>().color = new Color(255, 251, 225, 255);
                     break;
